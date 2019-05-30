bdda0bac36b80ddbd038c677e0eb7c2efe3085a8
Dmytro Ovcharov <dmytro.ovcharov@aploris.com>
2019-05-30 10:16:07 +0300

    cool

---
+++ mkbundleExample/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace mkbundleExample
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var test1 = new ClassLibrary1.Class1();
+            var test2 = new ClassLibrary2.Class1();
+        }
+    }
+}
